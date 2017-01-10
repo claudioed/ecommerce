dfcad2ee448df9d22704476c6f0a2a0dd8c3af78
fernandofazolli <fernandofazolli@gmail.com>
2017-01-09 23:55:04 -0200

    configurando o projeto de configuração

---
+++ sandbox/src/main/java/com/ecommerce/sandbox/starter/SandBoxConfigApplication.java
@@ -0,0 +1,34 @@
+package com.ecommerce.sandbox.starter;
+
+/**
+ * Created by fernandofazolli on 09/01/17.
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SandBoxConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandBoxConfigApplication.class, args);
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
+
