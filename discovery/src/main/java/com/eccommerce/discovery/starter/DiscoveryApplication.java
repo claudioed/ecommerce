1b3f13acba7cc2df8339dfb89326ce1dc5a8107a
fernandofazolli <fernandofazolli@gmail.com>
2017-01-12 00:11:12 -0200

    configurando o projeto de discovery

---
+++ discovery/src/main/java/com/eccommerce/discovery/starter/DiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.eccommerce.discovery.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by fernandofazolli on 11/01/17.
+ */
+@SpringCloudApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+    public static void main(String... args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
